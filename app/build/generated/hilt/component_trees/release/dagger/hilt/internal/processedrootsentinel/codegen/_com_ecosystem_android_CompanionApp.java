896c176e05c96438bf0a45dbf94cd74879440895
vigneshchowdary31-debug <vigneshchowdary31@gmail.com>
2026-06-12 03:17:16 +0530

    chore(git): harden repository and add security exclusions

--- app/build/generated/hilt/component_trees/release/dagger/hilt/internal/processedrootsentinel/codegen/_com_ecosystem_android_CompanionApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.ecosystem.android.CompanionApp"
-)
-public final class _com_ecosystem_android_CompanionApp {
-}
